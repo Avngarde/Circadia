9c9540c751cb02943bd3cc8a902da1971435039d
Avngarde <pivotguy888@gmail.com>
2026-07-18 12:33:00 +0200

    Add: Init commit

---
+++ Circadia/CircadiaApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Circadia;
+
+public class CircadiaApplicationContext : ApplicationContext
+{
+    private NotifyIcon _trayIcon;
+
+    public CircadiaApplicationContext()
+    {
+        var menu = new ContextMenuStrip();
+        menu.Items.Add("Exit", null, Exit);
+
+        _trayIcon = new NotifyIcon
+        {
+            Icon = new Icon("./Resources/icon.ico"),
+            ContextMenuStrip = menu,
+            Visible = true
+        };
+    }
+
+    private void Exit(object? sender, EventArgs e)
+    {
+        _trayIcon.Visible = false;
+        _trayIcon.Dispose();
+        ExitThread();
+    }
+}
