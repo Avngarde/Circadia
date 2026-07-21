ac388b4520f13f4985265f7fec0a47a1492f7b17
Avngarde <pivotguy888@gmail.com>
2026-07-21 16:46:20 +0200

    Add: Changing system theme feature

---
+++ Circadia/Features/ISystemTheme.cs
@@ -0,0 +1,12 @@
+﻿namespace Circadia.Features;
+
+public interface ISystemTheme
+{
+    public void SetTheme(SystemThemeOption theme);
+}
+
+public enum SystemThemeOption
+{
+    Light,
+    Dark
+}
---
+++ Circadia/Features/SystemTheme.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+
+namespace Circadia.Features;
+
+public class SystemTheme : ISystemTheme
+{
+    private const string _key =
+        @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+    
+    public void SetTheme(SystemThemeOption theme)
+    {
+        using RegistryKey registryKey =
+            Registry.CurrentUser.CreateSubKey(_key);
+        
+        registryKey.SetValue("AppsUseLightTheme", theme == SystemThemeOption.Light);
+    }
+}
