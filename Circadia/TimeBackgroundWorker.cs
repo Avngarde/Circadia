f4ebde6a704c5c9f50ea6cf7362268606e933ab2
Avngarde <pivotguy888@gmail.com>
2026-09-15 17:45:50 +0200

    Fix: Fix errors in changing theme tracking algorithm

--- Circadia/TimeBackgroundWorker.cs
+++ Circadia/TimeBackgroundWorker.cs
@@ -13,17 +13,20 @@ public class TimeBackgroundWorker : BackgroundService
     private IBrightness _brightness;
     private ISystemTheme _theme;
     private IBlueLight _blueLight;
+    private SystemThemeOption _currentTheme;
 
     public TimeBackgroundWorker()
     {
         _theme = new SystemTheme();
         _brightness = new Brightness();
-        _blueLight = new BlueLight();    
+        _blueLight = new BlueLight();   
+
+        _currentTheme = _theme.GetTheme(); 
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using var timer = new PeriodicTimer (TimeSpan.FromMinutes(1));
+        using var timer = new PeriodicTimer (TimeSpan.FromSeconds(5));
 
         while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
         {
@@ -42,23 +45,33 @@ public class TimeBackgroundWorker : BackgroundService
     {
         var settings = Settings.Load();
         var timeNow = new TimeOnly(DateTime.Now.Hour, DateTime.Now.Minute);
+        bool isDarkMode;
 
-        bool isDarkMode = settings.DarkModeFrom < settings.DarkModeTo
-            ? timeNow >= settings.DarkModeFrom && timeNow < settings.DarkModeTo
-            : timeNow >= settings.DarkModeFrom || timeNow < settings.DarkModeTo;
-        
+        if (settings.DarkModeFrom > settings.DarkModeTo) // Clocks moves past 00:00 during the dark mode span
+            isDarkMode = timeNow > settings.DarkModeFrom || timeNow < settings.DarkModeTo;
+        else
+            isDarkMode = timeNow > settings.DarkModeFrom && timeNow < settings.DarkModeTo;
 
+        if ((isDarkMode && _currentTheme == SystemThemeOption.Dark) 
+            || 
+           (!isDarkMode && _currentTheme == SystemThemeOption.Light))
+            return;
+    
         if (isDarkMode)
         {
             _brightness.SetBrightness((uint)settings.BrightnessDark);
             _theme.SetTheme(SystemThemeOption.Dark);
             _blueLight.TurnOn((uint)settings.BlueLightValue);
+
+            _currentTheme = SystemThemeOption.Dark;
         }
         else
         {
             _brightness.SetBrightness((uint)settings.BrightnessLight);
             _theme.SetTheme(SystemThemeOption.Light);
-            _blueLight.TurnOff();           
+            _blueLight.TurnOff(); 
+
+            _currentTheme = SystemThemeOption.Light;         
         }
     }
 }
